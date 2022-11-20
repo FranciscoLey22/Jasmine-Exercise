805c0b7cb822f89c9f1d338b68b7d7dbd00f4eac
Francisco Ley <francisco.ley@gmail.com>
2022-11-20 15:42:19 -0500

    ran some Jasmine tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,23 @@
 
 it('should calculate the monthly rate correctly', function () {
-  expect
+  const values = {
+    amount: 200000,
+    years: 30,
+    rate: 3.25
+  };
+  
+  expect(calculateMonthlyPayment(values)).toEqual("870.00");
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  const values = {
+    amount: 200000,
+    years: 30,
+    rate: 3.25
+  };
+  expect(calculateMonthlyPayment(values)).toEqual("870.41")
 });
 
 /// etc
