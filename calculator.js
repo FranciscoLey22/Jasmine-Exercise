c8a3bca472c63b05193a556d8e6402d7cc241dd3
Francisco Ley <francisco.ley@gmail.com>
2022-11-19 23:00:59 -0500

    entered variables for loan amount, term, and interest rate in JS and also tried to enter code for the payment calculation

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
-    setupIntialValues();
+    setupInitialValues();
     form.addEventListener("submit", function(e) {
       e.preventDefault();
       update();
@@ -9,23 +9,40 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+
 function getCurrentUIValues() {
   return {
     amount: +(document.getElementById("loan-amount").value),
     years: +(document.getElementById("loan-years").value),
     rate: +(document.getElementById("loan-rate").value),
+
   }
 }
 
 // Get the inputs from the DOM.
+//amount, term, rate
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
-function setupIntialValues() {
+function setupInitialValues() {
+ 
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+const values = getCurrentUIValues();
+const P = values.amount;
+const i = values.rate /12;
+const n = 12 * values.years;
+console.log(P, i, n);
+// console.log(i*(1+i)**n);
+// console.log((1+i)**n - 1);
+ //const payment = P * (((i*(1+i)**n)) / ((1+i)**n - 1));
+ const payment = (P * i) / (1 - ((1+i)**(-n)));
+ console.log(payment);
+// console.log(values.amount);
+// console.log(values.years);
+// console.log(values.rate);
 }
 
 // Given an object of values (a value has amount, years and rate ),
